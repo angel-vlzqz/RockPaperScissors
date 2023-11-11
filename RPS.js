a1bfd4dea514b873804371db93f1945fbe2da58a
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-11-11 13:40:37 -0800

    implement game function which loops over playround 5 times

--- RPS.js
+++ RPS.js
@@ -10,32 +10,39 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      return "You lose! Paper beats rock.";
-    } else {
-      return "You win! Rock beats scissors.";
-    }
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-      return "You win! Paper beats rock.";
-    } else {
-      return "You lose! Scissors beats paper.";
-    }
-  } else if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      return "You lose! Rock beats scissors.";
-    } else {
-      return "You win! Scissors beats paper.";
-    }
-  }
-}
-
 function getPlayerChoice() {
   return prompt("Rock, paper, or scissors?").toLowerCase();
 }
 
-prompt(playRound(getPlayerChoice(), getComputerChoice()));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+    function playRound(playerSelection, computerSelection) {
+      if (playerSelection === computerSelection) {
+        return "It's a tie!";
+      } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+          return "You lose! Paper beats rock.";
+        } else {
+          return "You win! Rock beats scissors.";
+        }
+      } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+          return "You win! Paper beats rock.";
+        } else {
+          return "You lose! Scissors beats paper.";
+        }
+      } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+          return "You lose! Rock beats scissors.";
+        } else {
+          return "You win! Scissors beats paper.";
+        }
+      }
+    }
+  }
+}
+
+game();
